5b7408e9de5f048c45d33092b1a1fcee060b7d87
godxvincent <godxvincent@gmail.com>
2022-02-07 09:10:04 -0500

    :sparkles: Se agrega paso de storing_user

--- repositorio/dags/user_processing.py
+++ repositorio/dags/user_processing.py
@@ -1,11 +1,16 @@
+import json
 from datetime import datetime
+from pandas import json_normalize
 
 from airflow.models import DAG
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 
 # Todas las tasks/operators will follow this parameters.
-default_args = {"start_date": datetime(2020, 1, 1)}
+default_args = {"start_date": datetime.today()}
 
 CREATE_TABLE_QUERY = """
     CREATE TABLE users (
@@ -17,6 +22,31 @@ CREATE_TABLE_QUERY = """
         password TEXT NOT NULL
     );
 """
+
+FILE_PATH = "/tmp/processed_users.csv"
+
+
+def _processing_users(ti):
+    users = ti.xcom_pull(task_ids=["extracting_user"])
+    print("Esto es lo que llega al pipeline")
+    print(users)
+    print("Fin del comunicado")
+    if not len(users) or "results" not in users[0]:
+        raise ValueError
+    else:
+        user = users[0]["results"][0]
+        user_processed = json_normalize(
+            {
+                "firstname": user["name"]["first"],
+                "lastname": user["name"]["last"],
+                "country": user["location"]["country"],
+                "username": user["login"]["username"],
+                "password": user["login"]["password"],
+                "email": user["email"],
+            }
+        )
+        user_processed.to_csv(FILE_PATH, index=None, header=False)
+
 
 # run once everyday
 with DAG(
@@ -32,4 +62,22 @@ with DAG(
 
     is_api_available = HttpSensor(
         task_id="is_api_available", http_conn_id="users_api", endpoint="api/"
+    )
+
+    extracting_user = SimpleHttpOperator(
+        task_id="extracting_user",
+        http_conn_id="users_api",
+        endpoint="api/",
+        method="GET",
+        response_filter=lambda response: json.loads(response.text),
+        log_response=True,
+    )
+
+    processing_users = PythonOperator(
+        task_id="processing_users", python_callable=_processing_users
+    )
+
+    storign_user = BashOperator(
+        task_id="storing_user",
+        bash_command=f"echo -e '.separator ','\n.import {FILE_PATH} users' | sqlite3 ~/airflow/airflow.db",
     )
