303e85833bb89d3f24c9c521e0e7894e9c426f94
godxvincent <godxvincent@gmail.com>
2022-02-10 16:30:05 -0500

    :alembic: Se ajusta parallel_dag.py para usar task_groups en lugar de subdags

--- repositorio/dags/parallel_dag.py
+++ repositorio/dags/parallel_dag.py
@@ -4,6 +4,7 @@ from airflow.models import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.subdag import SubDagOperator
 from subdags.subdag_parallel_dag import subdag_parallel_dag
+from airflow.utils.task_group import TaskGroup
 
 default_args = {"start_date": datetime(2022, 2, 1)}
 MAIN_DAG = "parallel_dag"
@@ -19,18 +20,28 @@ with DAG(
         bash_command='sleep 3 | echo "task 1 executed"',
     )
 
-    SUBDAG_PROCESSING = "processing_tasks"
-    processing = SubDagOperator(
-        task_id=SUBDAG_PROCESSING,
-        subdag=subdag_parallel_dag(
-            MAIN_DAG, SUBDAG_PROCESSING, default_args=default_args
-        ),
-    )
+    # SUBDAG_PROCESSING = "processing_tasks"
+    # processing = SubDagOperator(
+    #     task_id=SUBDAG_PROCESSING,
+    #     subdag=subdag_parallel_dag(
+    #         MAIN_DAG, SUBDAG_PROCESSING, default_args=default_args
+    #     ),
+    # )
+
+    with TaskGroup("processing_tasks_group") as processing_tasks_group:
+        BashOperator(
+            task_id="task_2",
+            bash_command='sleep 3 | echo "task 2 executed"',
+        )
+        BashOperator(
+            task_id="task_3",
+            bash_command='sleep 3 | echo "task 3 executed"',
+        )
 
     task_4 = BashOperator(
         task_id="task_4",
         bash_command='sleep 3 | echo "task 4 executed"',
     )
 
-    task_1.set_downstream(processing)
-    task_4.set_upstream(processing)
+    task_1.set_downstream(processing_tasks_group)
+    task_4.set_upstream(processing_tasks_group)
